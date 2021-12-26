283b12265a6f35ff1d8242ee46da6de86233622e
Admin <Admin@ADMIN->
2021-12-26 20:18:33 +0700

    Добавьте файлы проекта.

---
+++ Modul1.Zadanie7.2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul1.Zadanie7._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите длину ребра куба = ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            double V, S;
+            GetParam(a, out V, out S);
+            Console.WriteLine("Площадь поверхности куба = {0}", S);
+            Console.WriteLine("Объем куба = {0}", V);
+            Console.ReadKey();
+        }
+        static void GetParam(int a, out double S, out double V)
+        {
+            S = 6 * (a * a);
+            V = a * a * a;
+        }
+
+    }
+}
